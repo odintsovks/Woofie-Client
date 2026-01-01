ec167d5e6b3b23b536471bade96d2a17cbcecc97
odintsovks <ksodintsov03@gmail.com>
2026-01-01 23:34:05 +0300

    flake: full qt6 environment

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
   outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    qtEnv = with pkgs.qt6; env "qt-custom-${qtbase.version}" (
+      [
+      ]
+    );
   in
   {
     devShell.${system} = pkgs.mkShell {
-      packages = with pkgs; [
-        qt6.full
+      buildInputs = with pkgs; [
+        qtEnv
         qtcreator
+        libGL
+        gdb
+        cmake
       ];
     };
   };
